022c87a38d9c44e694909eb3ee77c54315205f05
David Starr <david@elegantcode.com>
2019-10-26 07:54:16 -0700

    Initial Commit

---
+++ before/NullObjectPattern/NullObject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NullObject.Entities;
+using NullObject.Services;
+using NullObject.View;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LearnerService learnerService = new LearnerService();
+            ILearner learner = learnerService.GetLearner(3);
+            
+            LearnerView view = new LearnerView(learner);
+            view.RenderView();
+        }
+    }
+}
